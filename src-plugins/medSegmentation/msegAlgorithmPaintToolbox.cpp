6447a296f09712717c433c119dada8491516e61d
Loic Cadour <loic.cadour@inria.fr>
2015-03-03 11:54:30 +0100

    [PaintTBx] Breaks the link between the saved mask and the new one

--- src-plugins/medSegmentation/msegAlgorithmPaintToolbox.cpp
+++ src-plugins/medSegmentation/msegAlgorithmPaintToolbox.cpp
@@ -529,6 +529,8 @@ AlgorithmPaintToolbox::AlgorithmPaintToolbox(QWidget *parent ) :
     connect(removeSeed_shortcut,SIGNAL(activated()),this,SLOT(removeSeed()));
     connect(addBrushSize_shortcut,SIGNAL(activated()),this,SLOT(increaseBrushSize()));
     connect(reduceBrushSize_shortcut,SIGNAL(activated()),this,SLOT(reduceBrushSize()));
+
+    maskHasBeenSaved = false;
 }
 
 AlgorithmPaintToolbox::~AlgorithmPaintToolbox()
@@ -673,6 +675,7 @@ void AlgorithmPaintToolbox::import()
 {
     setOutputMetadata(m_imageData, m_maskData);
     medDataManager::instance()->importData(m_maskData, true);
+    maskHasBeenSaved = true;
 }
 
 void AlgorithmPaintToolbox::setWorkspace(medAbstractWorkspace* workspace)
@@ -752,6 +755,12 @@ void AlgorithmPaintToolbox::clearMask()
         m_itkMask->SetPipelineMTime(m_itkMask->GetMTime());
 
         m_maskAnnotationData->invokeModified();
+
+        if(maskHasBeenSaved)
+        {
+            m_imageData->removeAttachedData(m_maskAnnotationData);
+            maskHasBeenSaved = false;
+        }
     }
 }
 
