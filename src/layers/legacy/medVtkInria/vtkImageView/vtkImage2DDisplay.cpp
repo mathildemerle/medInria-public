8697d1ba8479623bd2f0f886e4347889fed81dcf
mathildemerle <mathilde.merle@ihu-liryc.fr>
2023-11-22 15:00:31 +0100

    [medVtkImageInfo] proper reset of a complex structure --sync (#796)
    * [medVtkImageInfo] proper reset of a complex structure (#1151)
    
    * [vtkImageInfo] an other way to reset the complex structure and please gcc (#1162)

--- src/layers/legacy/medVtkInria/vtkImageView/vtkImage2DDisplay.cpp
+++ src/layers/legacy/medVtkInria/vtkImageView/vtkImage2DDisplay.cpp
@@ -58,7 +58,7 @@ void vtkImage2DDisplay::SetInputData(vtkImageData *pi_poVtkImage)
     }
     else
     {
-        memset(&m_sVtkImageInfo, 0, sizeof(m_sVtkImageInfo));
+        memset(reinterpret_cast<void*>(&m_sVtkImageInfo), 0, sizeof(m_sVtkImageInfo));
     }
 }
 
