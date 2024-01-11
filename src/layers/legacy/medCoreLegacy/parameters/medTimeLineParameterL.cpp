441f49ef49431806eb601cd74e11a03d781bbc67
mathildemerle <mathilde.merle@ihu-liryc.fr>
2024-01-11 10:37:12 +0100

    [Close] 4D+3D data in view crash at app closing (#803)
    * [Close] 4D+3D data in view crash at app closing
    
    * [Parameters] solve mem leaks

--- src/layers/legacy/medCoreLegacy/parameters/medTimeLineParameterL.cpp
+++ src/layers/legacy/medCoreLegacy/parameters/medTimeLineParameterL.cpp
@@ -148,6 +148,7 @@ medTimeLineParameterL::medTimeLineParameterL(QString name, QObject *parent):
 
 medTimeLineParameterL::~medTimeLineParameterL()
 {
+    emit aboutToBeDestroyed();
     delete d;
 }
 
@@ -277,7 +278,6 @@ void medTimeLineParameterL::setDuration(const double& timeDuration)
         d->timeBetweenFrames = 0;
 
     d->timeLine->setDuration(timeDuration*1000);
-
 }
 
 void medTimeLineParameterL::setFrame(int frame)
