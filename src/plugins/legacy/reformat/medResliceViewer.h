cfe29544ba5f9a94ad4b0b41debe0894d3e33005
mathildemerle <mathilde.merle@ihu-liryc.fr>
2024-07-12 12:03:05 +0200

    [VTK9.2.6] switch to more recent VTK and ITK5.4rc04 -- 4.1 (#860)
    * [VTK9.0.1] base to switch medInria on it
    
    * [VTK9] compilation debug
    
    * [VTK9] first step reslice
    
    * [VTK9] switch to 9.2.6 and debug vtk bug in Reslice tlbx
    
    * [reslice] put back old copyright
    
    * [VTK] rebase code on master and adapt to v9.2.6
    
    * [VTK9.2.6] error with vtkProbeFilter on vtk8 or vtk9 data, switch to manual interpolation
    
    * [Startup] remove unnecessay methods/var
    
    * [VTK9] python version and vtk modules
    
    * [VTK9] vtk modules in medVtkInria
    
    * [VTK9] vtk ptr cleaning
    
    * [VTK9] module for windows and nodiscard method
    
    * [Unix] remove vtk qt parameters
    
    * [VTK9] module enable GUISupportQt
    
    * [VTK9] module enable renderingQt
    
    * [VisualStudio] avoid ambiguous call to overloaded isnan method
    
    * [VTK9] Qt 5
    
    * [ROOT] last package_DIR to package_ROOT
    
    * [TTK] remove real_path to TTK
    
    * [VTK9] remove include_directories()
    
    * [VTK9] fix problem with non allocated var
    
    * [TTK] change path to new branch

--- src/plugins/legacy/reformat/medResliceViewer.h
+++ src/plugins/legacy/reformat/medResliceViewer.h
@@ -12,6 +12,7 @@ PURPOSE.
 
 =========================================================================*/
 
+
 #include "resliceToolBox.h"
 
 #include <medAbstractView.h>
@@ -21,7 +22,7 @@ PURPOSE.
 
 #include <itkImage.h>
 
-#include <QVTKOpenGLWidget.h>
+#include <QVTKOpenGLNativeWidget.h>
 
 #include <vtkImagePlaneWidget.h>
 #include <vtkResliceImageViewer.h>
@@ -53,11 +54,6 @@ public:
 public slots:
 
     virtual void thickMode(int);
-    virtual void blendMode(int);
-    virtual void SetBlendModeToMaxIP();
-    virtual void SetBlendModeToMinIP();
-    virtual void SetBlendModeToMeanIP();
-    virtual void SetBlendMode(int);
     virtual void reset();
     virtual void resetViews();
     virtual void render();
@@ -83,7 +79,7 @@ protected:
     vtkSmartPointer<vtkImagePlaneWidget> planeWidget[3];
     double planeNormal[3][3];
     QWidget *viewBody;
-    QVTKOpenGLWidget *views[4];
+    QVTKOpenGLNativeWidget *views[4];
     dtkSmartPointer<medAbstractData> inputData;
     std::array<double, 3> outputSpacingOrSize;
     unsigned char selectedView;
